6e0269f0e23cb386f15d09c1dd86c5b72e44a600
pldzhorev <pldzhorev@axway.com>
2024-11-14 16:47:47 +0200

    Removed MiniDexed related file in build_sd.sh

--- build_sd.sh
+++ build_sd.sh
@@ -21,7 +21,7 @@ cp ./src/kernel*.img ./kernels/
 # Make zip that contains Raspberry Pi 4 boot files. The contents can be copied to a FAT32 formatted partition on a microSD card
 # cp -r ./circle-stdlib/libs/circle/boot/* sdcard
 # rm -rf sdcard/config*.txt sdcard/README sdcard/Makefile sdcard/armstub sdcard/COPYING.linux
-# cp ./src/config.txt ./src/minijv880.ini ./src/*img ./src/performance.ini sdcard/
+# cp ./src/config.txt ./src/minijv880.ini ./src/*img sdcard/
 # echo "usbspeed=full" > sdcard/cmdline.txt
 # cd sdcard
 # cp ../kernels/* . || true
